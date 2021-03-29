5eed2b099af6f0551645d3e676d7d8702d884f88
Olli Hirvonen <ollihirvonen@gmail.com>
2021-03-29 16:05:50 -0400

    Initial Commit

---
+++ ShedApp/ShedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShedAppApp.swift
+//  ShedApp
+//
+//  Created by Olli Hirvonen on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ShedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
